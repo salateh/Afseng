1a2cc859a1d534a3ddcd29d915ae184b3d552408
SalatEh <234914038+salateh@users.noreply.github.com>
2026-05-30 18:40:06 +0300

    feature: implement main application layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import { useState } from "react";
-import { useHttp } from "./hooks/useHttp";
-import type { Exercise } from "./types/exercises.types";
+import { Route, Routes } from "react-router";
+import { MainLayout } from "./layouts/MainLayout";
 
 export default function App() {
-  const [page, setPage] = useState<number>(1);
-  const { data, error } = useHttp<Exercise>("exercises", page, 2);
-  console.log("page:", page);
-  console.log("data:", data);
-
   return (
-    <>
-      <p>{data?.results.map((a) => a.name)}</p>
-      {error && <p>{error}</p>}
-      <button onClick={() => setPage((prev) => prev + 1)}>next page</button>
-    </>
+    <div className=" bg-gradient-to-r from-stone-700 via-stone-900/95 to-stone-700">
+      <Routes>
+        <Route path="/" element={<MainLayout />}></Route>
+      </Routes>
+    </div>
   );
 }
---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Outlet } from "react-router";
+import "./layout.style.css";
+
+export function MainLayout() {
+  return (
+    <div className="grid min-h-screen grid-areas-layout grid-cols-1 md:grid-cols-[180px_1fr] grid-rows-[auto_1fr_auto] ">
+      <header className="[grid-area:header]   bg-gradient-to-r from-zinc-900/93 via-stone-900/82 to-zinc-900/93  flex items-center justify-between  shadow-md p-4 z-10 sticky top-0  "></header>
+      <aside className="[grid-area:sidebar] hidden md:block bg-slate-600  h-screen  p-5 sticky top-0 z-10 shadow-md "></aside>
+
+      <main className="[grid-area:main]  bg-stone-800 p-6">
+        <Outlet />
+      </main>
+
+      <footer className="[grid-area:footer]  bg-zinc-950 p-6 z-10 text-white flex  items-center justify-evenly  "></footer>
+    </div>
+  );
+}
